1a0fa212ae86beb24ff4ec3eda76a0b85245c358
zepoze <me@zepoze.fr>
2026-03-02 13:07:58 +0100

    chore: IaC: Download user_ca.pub and use it if present from s3 artefact bucket for instance

--- terraform/deploy.sh
+++ terraform/deploy.sh
@@ -13,6 +13,15 @@ fi
 
 sudo aws s3 cp s3://$ARTEFACT_BUCKET_FOLDER/slides.md "$(dirname "$0")/slides/slides.md"
 
+TRUSTED_USERS_CA_FILE="$(dirname "$0")/proxy/users_ca.pub"
+rm -f "$TRUSTED_USERS_CA_FILE" 2> /dev/null || true
+sudo aws s3 cp s3://$ARTEFACT_BUCKET_FOLDER/users_ca.pub "$TRUSTED_USERS_CA_FILE" 2> /dev/null || true
+
+if [ -f "$TRUSTED_USERS_CA_FILE" ]; then
+    sudo chmod 644 "$TRUSTED_USERS_CA_FILE"
+    export SSH_TRUSTED_USERS_CA="/workspace/users_ca.pub"
+fi
+
 aws ecr get-login-password --region "$REGION" | docker login --username AWS --password-stdin "$ECR_REGISTRY_SLIDES"
 aws ecr get-login-password --region "$REGION" | docker login --username AWS --password-stdin "$ECR_REGISTRY_PROXY"
 
